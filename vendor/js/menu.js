7c4152cbf2f1b052e1e15e453ee2773d61ed2df9
Katja Pott <katja.pott@students.fhnw.ch>
2017-12-28 11:00:17 +0100

    menu

--- vendor/js/menu.js
+++ vendor/js/menu.js
@@ -7,13 +7,13 @@ $(document).ready(function() {
 
     $('.parent2').on('mousedown touchstart', function() {
 
-        if (!active1) $(this).find('.test1').css({'background-color': 'gray', 'transform': 'translate(0px,125px)'});
+        if (!active1) $(this).find('.test1').css({'background-color': 'transparent', 'transform': 'translate(0px,125px)'});
         else $(this).find('.test1').css({'background-color': 'dimGray', 'transform': 'none'});
-        if (!active2) $(this).find('.test2').css({'background-color': 'gray', 'transform': 'translate(60px,105px)'});
+        if (!active2) $(this).find('.test2').css({'background-color': 'transparent', 'transform': 'translate(60px,105px)'});
         else $(this).find('.test2').css({'background-color': 'darkGray', 'transform': 'none'});
-        if (!active3) $(this).find('.test3').css({'background-color': 'gray', 'transform': 'translate(105px,60px)'});
+        if (!active3) $(this).find('.test3').css({'background-color': 'transparent', 'transform': 'translate(105px,60px)'});
         else $(this).find('.test3').css({'background-color': 'silver', 'transform': 'none'});
-        if (!active4) $(this).find('.test4').css({'background-color': 'gray', 'transform': 'translate(125px,0px)'});
+        if (!active4) $(this).find('.test4').css({'background-color': 'transparent', 'transform': 'translate(125px,0px)'});
         else $(this).find('.test4').css({'background-color': 'silver', 'transform': 'none'});
         active1 = !active1;
         active2 = !active2;
